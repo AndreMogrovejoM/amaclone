3767c93e7a498fee21c3abf46582960b2ad4afda
Skdiy <amogrovejom@unsa.edu.pe>
2021-04-14 14:12:35 -0500

    header and first home screen

---
+++ src/backend/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA_Z5col5kaLrRwAuCm3qkEiHIqkMOev2k",
+    authDomain: "ama-clone-33e50.firebaseapp.com",
+    projectId: "ama-clone-33e50",
+    storageBucket: "ama-clone-33e50.appspot.com",
+    messagingSenderId: "327642374201",
+    appId: "1:327642374201:web:76bfc87728e86f5e972de8",
+    measurementId: "G-MKXXRF9DMP"
+  };
